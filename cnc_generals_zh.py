4c11d3bd620605ab5ac51eb4541d95631c9a6f22
CaptainMcGee <cptmariomcgee@gmail.com>
2025-05-16 21:09:18 -0500

    Finalized C&C Generals Zero Hour

--- cnc_generals_zh.py
+++ cnc_generals_zh.py
@@ -5,7 +5,7 @@ from typing import List, Dict, Set
 
 from dataclasses import dataclass
 
-from Options import Toggle, OptionSet
+from Options import Toggle, OptionSet, NamedRange, TextChoice, Range
 
 from ..game import Game
 from ..game_objective_template import GameObjectiveTemplate
@@ -16,17 +16,14 @@ from ..enums import KeymastersKeepGamePlatforms
 # Option Dataclass
 @dataclass
 class CNCGeneralsZHArchipelagoOptions:
-    cncgeneralszh_min_skirmish_wins: CNCGeneralsZHMinSkirmishWins
-    cncgeneralszh_max_skirmish_wins: CNCGeneralsZHMaxSkirmishWins
-    cncgeneralszh_ai_difficulty: CNCGeneralsZHDifficulty
-    cncgeneralszh_max_skirmish_map_size: CNCGeneralsZHMaxSkirmishMapSize
-    cncgeneralszh_allow_custom_maps: CNCGeneralsZHAllowCustomMaps
-    cncgeneralszh_include_campaign: CNCGeneralsZHIncludeCampaign
-    cncgeneralszh_include_challenge: CNCGeneralsZHIncludeChallenge
-    cncgeneralszh_limit_structures: CNCGeneralsZHLimitStructures
-    cncgeneralszh_limit_structures_amount: CNCGeneralsZHLimitStructuresAmount
-    cncgeneralszh_limit_units: CNCGeneralsZHLimitUnits
-    cncgeneralszh_limit_units_amount: CNCGeneralsZHLimitUnitsAmount
+    generals_zh_ai_difficulty: CNCGeneralsZHDifficulty
+    generals_zh_max_skirmish_map_size: CNCGeneralsZHMaxSkirmishMapSize
+    generals_zh_include_campaign: CNCGeneralsZHIncludeCampaign
+    generals_zh_include_challenge: CNCGeneralsZHIncludeChallenge
+    generals_zh_limit_structures: CNCGeneralsZHLimitStructures
+    generals_zh_limit_structures_amount: CNCGeneralsZHLimitStructuresAmount
+    generals_zh_limit_units: CNCGeneralsZHLimitUnits
+    generals_zh_limit_units_amount: CNCGeneralsZHLimitUnitsAmount
 
 # Main Class
 class CNCGeneralsZHGame(Game):
@@ -41,22 +38,33 @@ class CNCGeneralsZHGame(Game):
 
     # Optional Game Constraints
     def optional_game_constraint_templates(self) -> List[GameObjectiveTemplate]:
-        if not self.banned_structures and not self.banned_units:
-            return list()
-            
+        constraints: List[GameObjectiveTemplate] = list()
+        if self.banned_structures and (self.archipelago_options.generals_zh_limit_structures_amount.value > 0):
+            constraints += self.structure_constraints()
+        if self.banned_units and (self.archipelago_options.generals_zh_limit_structures_amount.value > 0):
+            constraints += self.unit_constraints()
+
+        return constraints
+    
+    def structure_constraints(self) -> List[GameObjectiveTemplate]:
+        # temp = self.num_banned_structures
         return [
             # Structures
             GameObjectiveTemplate(
                 label="Cannot build these Structures: STRUCTURES",
                 data={
-                    "STRUCTURES": (self.banned_structures, self.num_banned_structures),
+                    "STRUCTURES": (self.structures, self.archipelago_options.generals_zh_limit_structures_amount.value),
                 },
             ),
+        ]
+    
+    def unit_constraints(self) -> List[GameObjectiveTemplate]:
+        return [
             # Units
             GameObjectiveTemplate(
                 label="Cannot produce these types of Units: UNITS",
                 data={
-                    "UNITS": (self.banned_units, self.num_banned_units),
+                    "UNITS": (self.units, self.archipelago_options.generals_zh_limit_units_amount.value),
                 },
             ),
         ]
@@ -66,27 +74,30 @@ class CNCGeneralsZHGame(Game):
     # Combine Objectives (Campaign and Challenges)
     def game_objective_templates(self) -> List[GameObjectiveTemplate]:
         objectives: List[GameObjectiveTemplate] = list()
-        if self.objective_battlememories:
+        objectives += self.skirmish_objectives()
+        if self.include_campaign:
             objectives += self.campaign_objectives()
-        if self.objective_gifts:
+        if self.include_challenge:
             objectives += self.challenge_objectives()
 
         return objectives
 
     # Skirmish Objectives
     def skirmish_objectives(self) -> List[GameObjectiveTemplate]:
+        # num_matches = 
         return [
             GameObjectiveTemplate(
-                label="Win a match against these DIFFICULTY armies (separately) as GENERAL, on MAP: AI_GENERALS",
+                label="Win MATCHES matches against a DIFFICULTY AI army, as GENERAL, on MAP",
                 data={
+                    "MATCHES": (self.matches, 1),
                     "DIFFICULTY": (self.difficulty, 1),
                     "GENERAL": (self.generals, 1),
                     "MAP": (self.maps, 1),
-                    "AI_GENERALS": (self.generals, range(self.min_wins, self.max_wins + 1)),
+                    "AI": (self.generals, 1),
                 },
                 is_time_consuming=False,
                 is_difficult=False,
-                weight=5,
+                weight=6,
             )
         ]
 
@@ -106,7 +117,7 @@ class CNCGeneralsZHGame(Game):
         ]
 
     # Challenge Objectives
-    def campaign_objectives(self) -> List[GameObjectiveTemplate]:
+    def challenge_objectives(self) -> List[GameObjectiveTemplate]:
         return [
             GameObjectiveTemplate(
                 label="Complete the GENERAL Challenge, on DIFFICULTY difficulty",
@@ -223,9 +234,11 @@ class CNCGeneralsZHGame(Game):
         maps: List[str] = self.maps_2[:]
 
         # Iteratively add maps based on max_map_size, starting from 3
-        for size in range(3, self.max_map_size + 1):
-            map_list = getattr(self, f'maps_{size}')
-            maps.extend(map_list[:])
+        if self.archipelago_options.generals_zh_max_skirmish_map_size.value >= 3:
+        #   maps.extend(self.maps_8) 
+            for size in range(3, self.archipelago_options.generals_zh_max_skirmish_map_size.value + 1):
+                map_list = getattr(self, f'maps_{size}')
+                maps.extend(map_list[:])
 
         return sorted(maps)
 
@@ -233,17 +246,17 @@ class CNCGeneralsZHGame(Game):
     @staticmethod
     def generals() -> List[str]:
         return [
-            "Any General"
-            "Random General"
-            "USA (No General)",
+            "**YOU PICK**",
+            "Random General",
+            "USA",
             "USA Super Weapon General",
             "USA Laser General",
             "USA Air Force General",
-            "China (No General)",
+            "China",
             "China Tank General",
             "China Infantry General",
             "China Nuke General",
-            "GLA (No General)",
+            "GLA",
             "GLA Toxin General",
             "GLA Demolition General",
             "GLA Stealth General",
@@ -273,199 +286,146 @@ class CNCGeneralsZHGame(Game):
             "GLA Stealth General's",
         ]
 
-    # Min Skirmish Wins
-    @property
-    def min_wins(self) -> int:
-        return int(self.archipelago_options.cncgeneralszh_min_skirmish_wins.value)
+    # Number of Matches
+    # @staticmethod
+    
+    # def num_matches() -> range:
+    #    return range(1, 4)
 
-    # Max Skirmish Wins
-    @property
-    def max_wins(self) -> int:
-        return int(self.archipelago_options.cncgeneralszh_max_skirmish_wins.value)
-
+    @staticmethod
+    def matches() -> range:
+        return range(1,4)
+    
     # AI Difficulty
     @property
-    def difficulty(self) -> str:
-        return str(self.archipelago_options.cncgeneralszh_ai_difficulty.value)
-
-    # Max Skirmish Map Size
-    @property
-    def max_map_size(self) -> int:
-        return int(self.archipelago_options.CNCGeneralsZHMaxSkirmishMapSize.value)
-
-    # Allow Custom Maps
-    @property
-    def include_custom(self) -> bool:
-        return bool(self.archipelago_options.CNCGeneralsZHMaxSkirmishMapSize.value)
+    def ai_difficulty(self) -> Set[str]:
+        return self.archipelago_options.generals_zh_ai_difficulty.value
+    
+    def difficulty(self) -> List[str]:
+        return sorted(self.ai_difficulty)
 
     # Include Campaign
     @property
     def include_campaign(self) -> bool:
-        return bool(self.archipelago_options.CNCGeneralsZHIncludeCampaign.value)
+        return self.archipelago_options.generals_zh_include_campaign
 
     # Include Challenge
     @property
     def include_challenge(self) -> bool:
-        return bool(self.archipelago_options.CNCGeneralsZHIncludeChallenge.value)
+        return self.archipelago_options.generals_zh_include_challenge
 
     # Banned Structures/Buildings
     @property
     def banned_structures(self) -> Set[str]:
-        return self.archipelago_options.CNCGeneralsZHLimitStructures.value
+        return self.archipelago_options.generals_zh_limit_structures.value
+    
+    def structures(self) -> List[str]:
+        return sorted(self.banned_structures)
 
     # Banned Units
     @property
     def banned_units(self) -> Set[str]:
-        return self.archipelago_options.CNCGeneralsZHLimitUnits.value
+        return self.archipelago_options.generals_zh_limit_units.value
+    
+    def units(self) -> List[str]:
+        return sorted(self.banned_units)
 
-    # Banned Structure/Building Amount
-    @property
-    def num_banned_structures(self) -> int:
-        return int(self.archipelago_options.CNCGeneralsZHLimitStructuresAmount.value)
 
-    # Banned Unit Amount
-    @property
-    def num_banned_units(self) -> int:
-        return int(self.archipelago_options.CNCGeneralsZHLimitUnitsAmount.value)
 
 # Archipelago Options
-class CNCGeneralsZHMinSkirmishWins(NamedRange):
+
+class CNCGeneralsZHDifficulty(OptionSet):
+    """Indicates the range of difficulties of Enemy AI armies in ALL gamemodes. Accepts multiple difficulty options. Defaults to just Normal.
+
+    Valid Difficulties:
+    - Easy
+    - Normal
+    - Hard
+
+    Example Input: ['Easy','Normal','Hard']
     """
-    Specifies the minimum number of wins needed for Skirmish matches when generating objectives.
-    Specify a number from 1-5 or pick from the named options below.
-    - normal: 1 win
-    - extreme: 3 wins
-    - extreme: 5 wins
-    - random: Randomized minimum from 1 to 5 wins
-    **DOES NOT CHECK IF MIN IS GREATER THAN MAX!**
+
+    valid_keys = [
+        "Easy",
+        "Normal",
+        "Hard",
+    ]
+
+    default = ["Normal"]
+
+    display_name = "Command and Conquer: Generals Zero Hour AI Difficulty"
+
+class CNCGeneralsZHMaxSkirmishMapSize(NamedRange):
     """
-
-    rich_text_doc = True
-    range_start = 1
-    range_end = 5
-    special_range_names = {
-        "normal": 1,
-        "long": 3,
-        "extreme": 5,
-        "random": random,
-    }
-
-    default = 1
-
-    display_name = "Command and Conquer: Generals Zero Hour Min Skirmish Wins"
-
-class CNCGeneralsZHMaxSkirmishWins(NamedRange):
-    """
-    Specifies the maximum number of wins needed for Skirmish matches when generating objectives.
-    Specify a number from 1-5 or pick from the named options below.
-    - normal: 1 win
-    - extreme: 3 wins
-    - extreme: 5 wins
-    - random: Randomized maximum from 1 to 5 wins
-    **DOES NOT CHECK IF MAX IS LESS THAN MIN!**
+    Indicates the max map size for Skirmish matches when generating objectives.
+    Range is from 2 to 8. Defaults to 2.
+    Assumes that each slot is filled with an AI army on Team None (or for multiplayer, equally sized teams as human players).
+    You can choose if the AI armies are all the same general as the one specified in the objective or just one of them are the specified AI general.
     """
 
     rich_text_doc = True
-    range_start = 1
-    range_end = 5
+    range_start = 2
+    range_end = 8
     special_range_names = {
-        "normal": 1,
-        "long": 3,
-        "extreme": 5,
-        "random": random,
+        "duel": 2,
+        "smallteams": 4,
+        "largeteams": 8,
     }
 
     default = 2
 
-    display_name = "Command and Conquer: Generals Zero Hour Max Skirmish Wins"
-
-class CNCGeneralsZHDifficulty(Choice):
-    """Sets difficulty of Enemy AI armies in ALL gamemodes. Defaults to Normal.
-
-    - **Easy:**
-    - **Normal:**
-    - **Hard:**"""
-    display_name = "Difficulty"
-    rich_text_doc = True
-    option_easy = 0
-    option_normal = 1
-    option_hard = 2
-    default = 1
-
-    display_name = "Command and Conquer: Generals Zero Hour AI Difficulty"
-
-class CNCGeneralsZHMaxSkirmishMapSize(Range):
-    """
-    Indicates the max map size for Skirmish matches when generating objectives.
-    Range is from 2 to 8. 
-    Assumes that each slot is filled with an AI army on Team None (or for multiplayer, equal sized teams as human players).
-    You can choose if the AI armies are all the same as the one specified in the objective or just one of them are the specified AI general.
-    """
-
     display_name = "Command and Conquer: Generals Zero Hour Max Skirmish Map Size"
-
-class CNCGeneralsZHAllowCustomMaps(Toggle):
-    """
-    Indicates whether to allow custom maps for Skirmish matches when generating objectives.
-    Custom Maps will have 50% weight for any Skirmish objectives.
-    """
-
-    display_name = "Command and Conquer: Generals Zero Hour Allow Custom Maps"
     
 class CNCGeneralsZHIncludeCampaign(Toggle):
     """
-    Indicates whether to include Challenge Missions (Missions 1-5, start to finish) when generating objectives.
+    Indicates whether to include Challenge Missions (Missions 1-5, start to finish) when generating objectives. Defaults to False.
     """
 
     display_name = "Command and Conquer: Generals Zero Hour Include Campaign Levels"
 
 class CNCGeneralsZHIncludeChallenge(Toggle):
     """
-    Indicates whether to include Challenge Missions (Missions 1-7, start to finish) when generating objectives.
+    Indicates whether to include Challenge Missions (Missions 1-7, start to finish) when generating objectives. Defaults to False.
     """
 
     display_name = "Command and Conquer: Generals Zero Hour Include Challenge Missions"
 
 class CNCGeneralsZHLimitStructures(OptionSet):
     """
-    Bans the use of a randomized building/structure when generating objectives.
+    POTENTIALLY bans the use of a randomized building/structure when generating objectives.
     Provide a List of strings for each building/structure you'd like to include as a potential ban.
     **DOES NOT CHECK FOR LOGICAL COMPLETION!**
 
     Valid Buildings/Structures:
-    - Barracks
-    - War Factory
     - Base Defenses
     - Air Field
-    - Strategy Buildings
-    - Alt. Money Buildings
+    - Strategy Buildings (Strategy Center, Propaganda Tower, Palace)
+    - Alt. Money Buildings (Supply Drop Zone, Internet Center, Black Market)
     - Superweapons
+
+    Example Input: ['Strategy Buildings','Alt. Money Buildings','Superweapons']
     """
 
-    rich_text_doc = True
-    valid_keys_casefold = True
     valid_keys = [
-        "Barracks",
-        "War Factory",
         "Base Defenses",
         "Air Field",
         "Strategy Buildings",
         "Alt. Money Buildings",
-        "Superweapons"
+        "Superweapons",
     ]
 
-    default = []
+    default = valid_keys
 
     display_name = "Command and Conquer: Generals Zero Hour Limit Structures"
 
 class CNCGeneralsZHLimitStructuresAmount(NamedRange):
     """
     Specifies the number of randomized building/structure bans when generating objectives.
-    Specify a number from 1-5, pick from the named options below, or **leave blank for no bans**.
+    Specify a number from 1-5, pick from the named options below, or **leave as is for no bans**.
+    - none: no bans
     - normal: 1 ban
     - extreme: 3 bans
-    - random: Randomized from 1 to 5 bans
-    Limited to up to 5 for some possibility of completion.
+    - impossible: 5 bans
     **DOES NOT CHECK FOR LOGICAL COMPLETION!**
     **DOES NOT CHECK IF PROVIDED NUMBER EXCEEDS PROVIDED LIST SIZE!**
     """
@@ -474,9 +434,10 @@ class CNCGeneralsZHLimitStructuresAmount(NamedRange):
     range_start = 1
     range_end = 5
     special_range_names = {
+        "none": 0,
         "normal": 1,
         "extreme": 3,
-        "random": random,
+        "impossible": 5,
     }
 
     default = 0
@@ -485,46 +446,47 @@ class CNCGeneralsZHLimitStructuresAmount(NamedRange):
 
 class CNCGeneralsZHLimitUnits(OptionSet):
     """
-    Bans the use of a randomized type of unit when generating objectives.
+    POTENTIALLY bans the use of a randomized type of unit when generating objectives.
     Provide a List of strings for each type of unit you'd like to include as a potential ban.
     **DOES NOT CHECK FOR LOGICAL COMPLETION!**
 
-    Valid Units:
-    - Rifle Infantry
-    - Rocket Infantry
-    - Standard Tanks
-    - Artillery + Avengers
-    - Plane-Type Aircraft
+    Valid Units (Detailed Descriptions within parentheses, don't put these in input)
+    - Rifle Infantry (Ranger, Red Guard, Mini-Gunner, Rebel)
+    - Rocket Infantry (Missile Defender, Tank Hunter, RPG Trooper)
+    - Infantry Vehicles (Humvee, Troop Crawler, Assault/Listening Outpost, Technical, Combat Cycle, Battle Bus)
+    - Tanks (Crusader, Laser Tank, Paladin, Battlemaster, Dragon Tank, Overlord, Scorpion, Marauder)
+    - Machine Gun Vehicles (Gatling Cannon, Quad Cannon)
+    - Artillery + Avengers (Tomahawk, Inferno Cannon, Nuke Cannon, SCUD Launcher, Avengers)
+    - Plane-Type Aircraft (Raptor, Stealth Figther, Aurora, MiG Fighter)
     - Comanches/Helixes
-    - Infantry Vehicles
-    - Machine Gun Vehicles
+
+    Example Input: ['Tanks','Artillery + Avengers','Plane-Type Aircraft','Infantry Vehicles']
     """
 
-    rich_text_doc = True
-    valid_keys_casefold = True
     valid_keys = [
         "Rifle Infantry",
         "Rocket Infantry",
-        "Standard Tanks",
+        "Infantry Vehicles",
+        "Tanks",
+        "Machine Gun Vehicles",
         "Artillery + Avengers",
         "Plane-Type Aircraft",
         "Comanches/Helixes",
-        "Infantry Vehicles",
-        "Machine Gun Vehicles"
     ]
 
-    default = []
+    default = valid_keys
 
     display_name = "Command and Conquer: Generals Zero Hour Limit Units"
 
 class CNCGeneralsZHLimitUnitsAmount(NamedRange):
     """
     Specifies the number of randomized unit bans when generating objectives.
-    Specify a number from 1-5, pick from the named options below, or **leave blank for no bans**.
+    Specify a number from 1-5, pick from the named options below, or **leave as is for no bans**.
+    - none: no bans
     - normal: 1 ban
     - extreme: 3 bans
-    - random: Randomized from 1 to 5 bans
-    Limited to up to 5 for some possibility of completion.
+    - impossible: 5 bans
+    Limited to up to 5.
     **DOES NOT CHECK FOR LOGICAL COMPLETION!**
     **DOES NOT CHECK IF PROVIDED NUMBER EXCEEDS PROVIDED LIST SIZE!**
     """
@@ -533,9 +495,10 @@ class CNCGeneralsZHLimitUnitsAmount(NamedRange):
     range_start = 1
     range_end = 5
     special_range_names = {
+        "none": 0,
         "normal": 1,
         "extreme": 3,
-        "random": random,
+        "impossible": 5,
     }
 
     default = 0
